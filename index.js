69e17c6ef4de8b0e7031f9368e0c6498536b584b
gliza03 <gabrielliza012@gmail.com>
2024-11-21 17:57:04 -0700

    Second commit

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.get("/memoryGame.ejs", (req, res) => res.render("memoryGame"));
 const knex = require("knex") ({
     client : "pg",
     connection : {
-        host : process.env.RDS_HOSTNAME || "localhost",
-        user : process.env.RDS_USERNAME || "postgres",
-        password : process.env.RDS_PASSWORD || "Gabbo2003#",
-        database : process.env.RDS_DB_NAME || "seminary",
-        port : process.env.RDS_PORT ||5432,
+        host : process.env.RDS_HOSTNAME,
+        user : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        database : process.env.RDS_DB_NAME,
+        port : process.env.RDS_PORT,
         ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 })
