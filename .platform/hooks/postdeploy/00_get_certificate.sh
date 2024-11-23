c7381ef33333b5e7e2485aa23a7ab0adf19a5794
gliza03 <99229900+gliza03@users.noreply.github.com>
2024-11-23 16:18:36 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d YOURDOMAINHERE --nginx --agree-tos --email YOUREMAILHERE
+sudo certbot -n -d http://test2imsotired.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email gabbolm@icloud.com
