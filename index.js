9eed6eb07a9f8cb8ed45610e6911518b138cf3e8
gabe <gabrielliza012@gmail.com>
2024-11-23 14:00:25 -0700

    ihml

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ app.get("/memoryGame.ejs", (req, res) => res.render("memoryGame"));
 const knex = require("knex") ({
     client : "pg",
     connection : {
-        host : process.env.RDS_HOSTNAME,
-        user : process.env.RDS_USERNAME,
-        password : process.env.RDS_PASSWORD,
-        database : process.env.RDS_DB_NAME,
-        port : process.env.RDS_PORT,
+        host : process.env.RDS_HOSTNAME || 'localhost',
+        user : process.env.RDS_USERNAME || 'postgres',
+        password : process.env.RDS_PASSWORD || 'Gabbo2003#',
+        database : process.env.RDS_DB_NAME || 'seminary',
+        port : process.env.RDS_PORT || '5432',
         ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 })
 
 app.get("/memoryGame.ejs", async (req, res) => { 
-    try { const student = await knex("student").select("student_code_name"); 
+    try { const student = await knex("student").select("student.student_code_name"); 
         res.render("memoryGame", { student });
      } catch (error) { 
         console.error("Error fetching students:", error); 
